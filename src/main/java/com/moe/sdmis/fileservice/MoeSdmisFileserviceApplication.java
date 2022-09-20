3ba56eca6f3829e0d3db6787fbbf35c0c48444f8
shamim <shamimahmad586@gmail.com>
2022-09-20 17:34:10 +0530

    My first Commit

---
+++ src/main/java/com/moe/sdmis/fileservice/MoeSdmisFileserviceApplication.java
@@ -0,0 +1,13 @@
+package com.moe.sdmis.fileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoeSdmisFileserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoeSdmisFileserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moe/sdmis/fileservice/MoeSdmisFileserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moe.sdmis.fileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoeSdmisFileserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
